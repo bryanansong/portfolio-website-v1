2bb72b7701ed9fe8b7a22c565f6866d4a660677f
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-01-04 03:07:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+	return (
+		<main className="relative bg-slate-900">
+			Nav
+			<section className="padding">Description</section>
+			<section className="padding">Experience</section>
+			<section className="padding">Projects</section>
+			<section className="padding">Footer</section>
+		</main>
+	);
+}
